9ce76a8f422bac24bd1a33f74940a41d524699c0
Pavell16 <117391127+Pavell16@users.noreply.github.com>
2023-03-07 19:55:21 +0100

    pulls ha Comand

---
+++ src/swing/pulls.java
@@ -0,0 +1,135 @@
+package swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import java.util.regex.Pattern;
+
+
+public class pulls extends JFrame implements ActionListener {
+    private JLabel title, Coins;
+    public String Cname = "", CGender = "", Canime = "", Cphoto = "", price="";
+    public int doraCoins = 0;
+    private static JTextField comands;
+    private JButton send;
+
+    public  String[][] characters = {
+        {"1", "Kim Dokja", "220", "Masculí", "Omniscient Reader's Viewpoint", "/swing/img/dokja.jpg"},
+        {"2", "Killua Zoldyck", "310", "Masculí", "Hunter x Hunter", "/swing/img/KilluaZoldyck_redim.jpg"},
+        {"3", "Kuroko Tetsuya", "180", "Masculí", "Kuroko no Basket", "/swing/img/KurokoTetsuya.jpg"},
+        {"4", "Klein Moretti", "200", "Masculí", "Lord Of The Mysteries", "/swing/img/KleinMoretti.jpg"},
+        {"5", "Lelouch Lamperouge", "310", "Masculí", "Code Geass", "/swing/img/LelouchLamperouge.jpg"},
+        {"6", "Saber", "570", "Femení", "Fate/Stay Night", "/swing/img/Saber.jpg"},
+        {"7", "Asta", "430", "Masculí", "Black Clover", "/swing/img/Asta.jpg"},
+        {"8", "Penelope Eckart", "80", "Femení", "Death Is the Only Ending for the Villainess", "/swing/img/PenelopeEckart.jpg"},
+        {"9", "Pyo Wol", "260", "Masculí", "Reaper of the Drifting Moon", "/swing/img/PyoWolredim.jpg"},
+        {"10", "Rick Sanchez", "50", "Masculí", "Rick y Morty", "/swing/img/RickSanchez.jpg"},
+        {"11", "Dunn Smith", "150", "Masculí", "Lord Of The Mysteries", "/swing/img/DunnSmith.jpg"},
+        {"12", "Sung Jin-Woo", "520", "Masculí", "Solo Leveling", "/swing/img/SunJinWoo.jpg"},
+        {"13", "Inuyasha", "140", "Masculí", "Inuyasha", "/swing/img/Inuyasha.jpg"},
+        {"14", "Sasha Braus", "210", "Femení", "Shingeki no Kyojin", "/swing/img/SashaBraus.jpg"},
+        {"15", "Nezuko Kamado", "600", "Femení", "Kimetsu no Yaiba", "/swing/img/NezukoKamado.jpg"},
+        {"16", "Winry Rockbell", "90", "Femení", "FullMetal Alchemist Brotherhood", "/swing/img/WinryRockbell.jpg"},
+        {"17", "Satoru Gojo", "590", "Masculí", "Jujutsu Kaisen", "/swing/img/SatoruGojo.jpg"},
+        {"18", "Edward Elric", "350", "Masculí", "FullMetal Alchemist Brotherhood", "/swing/img/EdwardElric.jpg"},
+        {"19", "Sukuna", "400", "Masculí", "Jujutsu Kaisen", "/swing/img/Sukuna.jpg"},
+        {"20", "Levi Ackerman", "500", "Masculí", "Shingeki no Kyojin", "/swing/img/LeviAckerman.jpg"}
+    };
+    public pulls(){
+        //Title Start
+        setLayout(null);
+        title = new JLabel("Pulls");
+        title.setFont(new Font("Tahoma", Font.BOLD, 25));
+        title.setBounds(230,5,270,75);
+        add(title);
+        //Title END
+
+        //comands
+        comands = new JTextField("$ha");
+        comands.setBounds(130, 100, 270,30);
+        add(comands);
+        //comands END
+
+        //Send
+        send = new JButton("Send");
+        send.setBounds(210, 150, 100, 30);
+        send.addActionListener(this);
+        add(send);
+        //Send END
+
+        //DoraCoins
+        Coins = new JLabel("DoraCoins" + doraCoins);
+        Coins.setBounds(400,5,100,30);
+        add(Coins);
+        //DoraCoins
+    }
+
+    public static void main(String args[]){
+        //Homepage
+        pulls Homepage = new pulls();
+        Homepage.setBounds(0,0,560,550);
+        Homepage.setResizable(false);
+        Homepage.setTitle("MudaePulls");
+        Homepage.setVisible(true);
+        Homepage.setLocationRelativeTo(null);
+        Homepage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Homepage
+
+    }
+
+
+    public void pullCharacter(){
+        Random random = new Random();
+        int characterpull = random.nextInt((20-1)+1)+1;
+
+        Cname = characters[characterpull-1][1];
+        price = characters[characterpull-1][2];
+        doraCoins = doraCoins + Integer.parseInt(characters[characterpull-1][2]);
+        Coins.setText("DoraCoins" + doraCoins);
+        CGender = characters[characterpull-1][3];
+        Canime = characters[characterpull-1][4];
+        ImageIcon icon = new ImageIcon(getClass().getResource(characters[characterpull-1][5]));
+        JLabel foto = new JLabel(icon);
+
+        // Jpanel per a combinar la iatge del personatge amb la seva informació
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(new JLabel("<html><b>Nom:</b> " + Cname + "<br>" +
+                "<b>DoraCoins:</b> "+ price + "<br>" +
+                "<b>Genere:</b> " + CGender + "<br>" +
+                "<b>Anime:</b> " + Canime + "</html>"), BorderLayout.CENTER);
+        panel.add(foto, BorderLayout.SOUTH);
+
+
+        JOptionPane.showMessageDialog(null, panel, "Pull",
+                JOptionPane.INFORMATION_MESSAGE);
+    };
+
+    public String getComand(){
+        String comand = comands.getText();
+        Pattern comandPattern = Pattern.compile("\\$");
+        String[] command = comandPattern.split(comand);
+
+
+        return command[1];
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == send){
+            String command = getComand();
+            switch (command){
+                case "ha":
+                    pullCharacter();
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Ho hi ha cap comanda així");
+                    break;
+            }
+
+
+
+        }
+    }
+}
